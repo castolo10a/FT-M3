1e23c0dba79f81f59ac34facde91a676cdbcecb9
Daniel Castillo <Daniel.302009@hotmail.com>
2023-02-07 11:10:14 -0500

    HW 01 NODE

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,6 +2,18 @@ const process = require("process");
 const commands = require("./commands/index.js");
 
 function bash() {
+  process.stdout.write("prompt > ");
+  process.stdin.on("data", (data) => {
+    let args = data.toString().trim().split(" ");
+    let cmd = args.shift()
+
+   commands[cmd] ? commands[cmd](print, args.join(" ")) : print(`command not found: ${cmd}`)
+  })
+}
+
+const print = (output) => {
+  process.stdout.write(output);
+  process.stdout.write("\nprompt > ");
 }
 
 bash();
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,63 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    print(Date())
+}
 
-function echo() {}
+function echo(print, args) {
+    print(args)
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir('.', (error, files) => {
+        if(error) throw new Error(error);
+        print(files.join(" "))
+    })
+}
 
-function cat() {}
+function cat(print, args) {
+    fs.readFile(args, "utf-8", (error, data) => {
+        if(error) throw new Error(error);
+        print(data)
+    })
+}
 
-function head() {}
+function head(print, args) {
+    fs.readFile(args, "utf-8", (error, data) => {
+        if(error) throw new Error(error);
 
-function tail() {}
+        print(data.split("\n")[0]);
+    })
+}
 
-function curl() {}
+function tail(print, args) {
+    fs.readFile(args, "utf-8", (error, data) => {
+        if(error) throw new Error(error);
 
-module.exports = {};
+        let line = data.split("\n")
+        print(line[line.length-1].trim())
+    })
+}
+
+function curl(print, args) {
+    utils.request(args, (error, response) => {
+        if(error) throw new Error(error);
+        print(response)
+    })
+}
+
+module.exports = {
+    pwd,
+    date,
+    echo,
+    ls,
+    cat,
+    head,
+    tail,
+    curl
+};
